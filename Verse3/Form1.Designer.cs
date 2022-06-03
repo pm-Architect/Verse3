88a131a4f346dfa1fe19dd5f1ae7c3239cb9c26c
Praneet Mathur <63034836+pm-Architect@users.noreply.github.com>
2022-06-03 16:47:34 +0530

    Initial commit

---
+++ Verse3/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace Verse3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.elementHost2 = new System.Windows.Forms.Integration.ElementHost();
+            this.canvasControl2 = new Verse3.CanvasCore.CanvasControl();
+            this.SuspendLayout();
+            // 
+            // elementHost2
+            // 
+            this.elementHost2.Location = new System.Drawing.Point(107, 52);
+            this.elementHost2.Name = "elementHost2";
+            this.elementHost2.Size = new System.Drawing.Size(454, 313);
+            this.elementHost2.TabIndex = 1;
+            this.elementHost2.Text = "elementHost2";
+            this.elementHost2.Child = this.canvasControl2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1022, 547);
+            this.Controls.Add(this.elementHost2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        private System.Windows.Forms.Integration.ElementHost elementHost2;
+        private CanvasCore.CanvasControl canvasControl2;
+
+        #endregion
+
+        //private System.Windows.Forms.Integration.ElementHost elementHost1;
+        //private CanvasControl canvasControl1;
+    }
+}
+
---
+++ Verse3/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Forms;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using Rectangle = System.Windows.Shapes.Rectangle;
+
+namespace Verse3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
