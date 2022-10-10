6cfbd838a22789f1702045ccdfe91d3db4641b68
aashwinshriv <aashwin@arpmdesignandresearch.com>
2022-10-10 18:04:20 +0200

    Min, Max, Sqrt

---
+++ MathLibrary/Maximum.cs
@@ -0,0 +1,111 @@
+﻿using Core;
+using System;
+using System.Windows;
+using Verse3;
+using Verse3.VanillaElements;
+
+namespace MathLibrary
+{
+    public class Maximum : BaseComp
+    {
+        public string? ElementText
+        {
+            get
+            {
+                string? name = this.GetType().FullName;
+                string? viewname = this.ViewType.FullName;
+                string? dataIN = "";
+                if (this.ComputationPipelineInfo.IOManager.DataOutputNodes != null && this.ComputationPipelineInfo.IOManager.DataOutputNodes.Count > 0)
+                    dataIN = (Math.Round((((NumberDataNode)this.ComputationPipelineInfo.IOManager.DataOutputNodes[0]).DataGoo.Data), 2)).ToString();
+                //string? zindex = DataViewModel.WPFControl.Content.
+                //TODO: Z Index control for IRenderable
+                return $"Name: {name}" +
+                    $"\nView: {viewname}" +
+                    $"\nID: {this.ID}" +
+                    $"\nX: {this.X}" +
+                    $"\nY: {this.Y}" +
+                    $"\nOutput Value: {dataIN}";
+            }
+        }
+
+        #region Properties
+
+
+        #endregion
+
+        #region Constructors
+
+        public Maximum() : base(0, 0)
+        {
+            //this.background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF6700"));
+            //Random rng = new Random();
+            //byte r = (byte)rng.Next(0, 255);
+            //this.backgroundTint = new SolidColorBrush(Color.FromArgb(100, r, r, r));
+        }
+
+        public Maximum(int x, int y, int width = 250, int height = 350) : base(x, y)
+        {
+            //base.boundingBox = new BoundingBox(x, y, width, height);
+
+            //Random rnd = new Random();
+            //byte rc = (byte)Math.Round(rnd.NextDouble() * 125.0);
+            //byte gc = (byte)Math.Round(rnd.NextDouble() * 125.0);
+            //byte bc = (byte)Math.Round(rnd.NextDouble() * 125.0);
+            //this.BackgroundTint = new SolidColorBrush(Color.FromRgb(rc, gc, bc));
+            //this.Background = new SolidColorBrush(Colors.Gray);
+        }
+
+        #endregion
+
+        public override void Compute()
+        {
+            double a = this.ChildElementManager.GetData<double>(0, 1);
+            double b = this.ChildElementManager.GetData<double>(1, 1);
+            this.ChildElementManager.SetData<double>((Math.Max(a, b)), 0);
+            textBlock.DisplayedText = this.ElementText;
+        }
+
+        public override CompInfo GetCompInfo()
+        {
+            Type[] types = { typeof(int), typeof(int), typeof(int), typeof(int) };
+            CompInfo ci = new CompInfo
+            {
+                ConstructorInfo = this.GetType().GetConstructor(types),
+                Name = "Maximum",
+                Group = "Advanced Operations",
+                Tab = "Math",
+                Description = "",
+                Author = "",
+                License = "",
+                Repository = "",
+                Version = "",
+                Website = ""
+            };
+            return ci;
+        }
+
+        private TextElement textBlock = new TextElement();
+        private NumberDataNode nodeBlock;
+        private NumberDataNode nodeBlock1;
+        private NumberDataNode nodeBlock2;
+        public override void Initialize()
+        {
+            nodeBlock = new NumberDataNode(this, NodeType.Input);
+            nodeBlock.Width = 50;
+            this.ChildElementManager.AddDataInputNode(nodeBlock, "A");
+
+            nodeBlock1 = new NumberDataNode(this, NodeType.Input);
+            nodeBlock1.Width = 50;
+            this.ChildElementManager.AddDataInputNode(nodeBlock1, "B");
+
+            nodeBlock2 = new NumberDataNode(this, NodeType.Output);
+            nodeBlock2.Width = 50;
+            this.ChildElementManager.AddDataOutputNode(nodeBlock2, "Result");
+
+            textBlock = new TextElement();
+            textBlock.DisplayedText = this.ElementText;
+            textBlock.TextAlignment = TextAlignment.Left;
+            this.ChildElementManager.AddElement(textBlock);
+        }
+    }
+}
