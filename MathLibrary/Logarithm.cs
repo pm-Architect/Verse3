4fba7a46a3ed24057fb9878ee86a18cf57e3e449
aashwinshriv <aashwin@arpmdesignandresearch.com>
2022-10-10 19:21:17 +0200

    Remainder, log, not gate

--- MathLibrary/Logarithm.cs
+++ MathLibrary/Logarithm.cs
@@ -3,11 +3,10 @@ using System;
 using System.Windows;
 using Verse3;
 using Verse3.VanillaElements;
-using TextElement = Verse3.VanillaElements.TextElement;
 
 namespace MathLibrary
 {
-    public class Division : BaseComp
+    public class Logarithm : BaseComp
     {
         public string? ElementText
         {
@@ -36,23 +35,33 @@ namespace MathLibrary
 
         #region Constructors
 
-        public Division() : base(0, 0)
+        public Logarithm() : base(0, 0)
         {
+            //this.background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF6700"));
+            //Random rng = new Random();
+            //byte r = (byte)rng.Next(0, 255);
+            //this.backgroundTint = new SolidColorBrush(Color.FromArgb(100, r, r, r));
         }
 
-        public Division(int x, int y, int width = 250, int height = 350) : base(x, y)
+        public Logarithm(int x, int y, int width = 250, int height = 350) : base(x, y)
         {
+            //base.boundingBox = new BoundingBox(x, y, width, height);
+
+            //Random rnd = new Random();
+            //byte rc = (byte)Math.Round(rnd.NextDouble() * 125.0);
+            //byte gc = (byte)Math.Round(rnd.NextDouble() * 125.0);
+            //byte bc = (byte)Math.Round(rnd.NextDouble() * 125.0);
+            //this.BackgroundTint = new SolidColorBrush(Color.FromRgb(rc, gc, bc));
+            //this.Background = new SolidColorBrush(Colors.Gray);
         }
 
         #endregion
 
         public override void Compute()
         {
-            double a = this.ChildElementManager.GetData<double>(0, 0);
+            double a = this.ChildElementManager.GetData<double>(0, 1);
             double b = this.ChildElementManager.GetData<double>(1, 1);
-            if (b == 0) return;
-            this.ChildElementManager.SetData<double>((a / b), 0);
-            this.ChildElementManager.SetData<double>((a % b), 1);
+            this.ChildElementManager.SetData<double>((Math.Log(a, b)), 0);
             textBlock.DisplayedText = this.ElementText;
         }
 
@@ -62,8 +71,8 @@ namespace MathLibrary
             CompInfo ci = new CompInfo
             {
                 ConstructorInfo = this.GetType().GetConstructor(types),
-                Name = "Division",
-                Group = "Operations",
+                Name = "Logarithm",
+                Group = "Advanced Operations",
                 Tab = "Math",
                 Description = "",
                 Author = "",
@@ -79,13 +88,12 @@ namespace MathLibrary
         private NumberDataNode nodeBlock;
         private NumberDataNode nodeBlock1;
         private NumberDataNode nodeBlock2;
-        private NumberDataNode nodeBlock3;
         public override void Initialize()
         {
             nodeBlock = new NumberDataNode(this, NodeType.Input);
             nodeBlock.Width = 50;
             this.ChildElementManager.AddDataInputNode(nodeBlock, "A");
-            
+
             nodeBlock1 = new NumberDataNode(this, NodeType.Input);
             nodeBlock1.Width = 50;
             this.ChildElementManager.AddDataInputNode(nodeBlock1, "B");
@@ -93,10 +101,6 @@ namespace MathLibrary
             nodeBlock2 = new NumberDataNode(this, NodeType.Output);
             nodeBlock2.Width = 50;
             this.ChildElementManager.AddDataOutputNode(nodeBlock2, "Result");
-
-            nodeBlock3 = new NumberDataNode(this, NodeType.Output);
-            nodeBlock3.Width = 50;
-            this.ChildElementManager.AddDataOutputNode(nodeBlock3, "Remainder");
 
             textBlock = new TextElement();
             textBlock.DisplayedText = this.ElementText;
