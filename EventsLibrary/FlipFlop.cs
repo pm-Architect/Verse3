78f99eb818b80e1ea5ab0a00d068195ecd52ac6a
aashwinshriv <aashwin@arpmdesignandresearch.com>
2022-10-18 15:15:33 +0200

    Flipflop

---
+++ EventsLibrary/FlipFlop.cs
@@ -0,0 +1,62 @@
+﻿using Core;
+using System;
+using System.Windows;
+using Verse3;
+using Verse3.VanillaElements;
+
+namespace EventsLibrary
+{
+    public class Flipflop : BaseComp
+    {
+       
+        public Flipflop() : base()
+        {
+        }
+        public Flipflop(int x, int y) : base(x, y)
+        {
+        }
+
+        public override void Compute()
+        {
+            
+        }
+        public override CompInfo GetCompInfo() => new CompInfo(this, "Flipflop", "Filters", "Events");
+        
+        private GenericEventNode eventIn;
+        private GenericEventNode eventTrue;
+        private GenericEventNode eventFalse;
+        private BooleanDataNode state;
+        public override void Initialize()
+        {
+            eventIn = new GenericEventNode(this, NodeType.Input);
+            eventIn.NodeEvent += eventIn_NodeEvent;
+            this.ChildElementManager.AddEventInputNode(eventIn);
+
+            eventTrue = new GenericEventNode(this, NodeType.Output);
+            this.ChildElementManager.AddEventOutputNode(eventTrue, "True");
+
+            eventFalse = new GenericEventNode(this, NodeType.Output);
+            this.ChildElementManager.AddEventOutputNode(eventFalse, "False");
+
+            state = new BooleanDataNode(this, NodeType.Output);
+            this.ChildElementManager.AddDataOutputNode(state, "State", true);
+        }
+
+        private void eventIn_NodeEvent(IEventNode container, EventArgData e)
+        {
+            bool _state = this.ChildElementManager.GetData(state, false);
+            _state = !_state;
+            this.previewTextBlock.DisplayedText = _state.ToString();
+            if (_state)
+            {
+                eventTrue.EventOccured(e);
+            }
+            else
+            {
+                eventFalse.EventOccured(e);
+            }
+            this.ChildElementManager.SetData<bool>(_state, state);
+            ComputationCore.Compute(this);
+        }
+    }
+}
