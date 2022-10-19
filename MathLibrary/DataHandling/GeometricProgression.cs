89469a1c66a198ee578204d503d49735c93f6dba
aashwinshriv <aashwin@arpmdesignandresearch.com>
2022-10-19 18:04:53 +0200

    text restructure + GP

--- MathLibrary/DataHandling/GeometricProgression.cs
+++ MathLibrary/DataHandling/GeometricProgression.cs
@@ -1,21 +1,21 @@
 ﻿using Core;
 using Verse3;
 using Verse3.VanillaElements;
-
+using System;
 
 namespace MathLibrary
 {
-    public class Series : BaseComp
+    public class GeometricProgression : BaseComp
     {
-        public Series() : base()
+        public GeometricProgression() : base()
         {
         }
-        public Series(int x, int y) : base(x, y)
+        public GeometricProgression(int x, int y) : base(x, y)
         {
         }
         
         private NumberDataNode Start;
-        private NumberDataNode Step;
+        private NumberDataNode Multiplier;
         private NumberDataNode Count;
         private NumberDataNode Result;
         public override void Initialize()
@@ -26,29 +26,30 @@ namespace MathLibrary
             Start = new NumberDataNode(this, NodeType.Input);
             this.ChildElementManager.AddDataInputNode(Start, "Start");
 
-            Step = new NumberDataNode(this, NodeType.Input);
-            this.ChildElementManager.AddDataInputNode(Step, "Step");
+            Multiplier = new NumberDataNode(this, NodeType.Input);
+            this.ChildElementManager.AddDataInputNode(Multiplier, "Multiplier");
 
             Count = new NumberDataNode(this, NodeType.Input);
             this.ChildElementManager.AddDataInputNode(Count, "Count");
 
             Result = new NumberDataNode(this, NodeType.Output);
-            this.ChildElementManager.AddDataOutputNode(Result, "AP", true);
+            this.ChildElementManager.AddDataOutputNode(Result, "GP", true);
         }
 
-        public override CompInfo GetCompInfo() => new CompInfo(this, "Arithmetic Progression", "Series", "Data");
+        public override CompInfo GetCompInfo() => new CompInfo(this, "Geometric Progression", "Series", "Data");
         
         public override void Compute()
         {
             double start = this.ChildElementManager.GetData(Start, 0.0);
-            double step = this.ChildElementManager.GetData(Step, 1.0);
-            int count = (int)this.ChildElementManager.GetData(Count, 10);
+            double multiplier = this.ChildElementManager.GetData(Multiplier, 1.0);
+            int count = (int)Math.Abs(this.ChildElementManager.GetData(Count, 10));
             DataStructure<double> result = new DataStructure<double>();
             for (int i = 0; i < count; i++)
             {
-                result.Add(start + i * step);
+                result.Add(start * Math.Pow(multiplier, i));
             }
             this.ChildElementManager.SetData(result, Result);
+            if (count > 0)
             this.previewTextBlock.DisplayedText = $"Last number = {result.Last.Value.Data.ToString()}";
         }
     }
