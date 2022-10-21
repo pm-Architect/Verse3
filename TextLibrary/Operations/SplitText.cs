be3534d66721031470d6f07ae10619270e92a3be
aashwinshriv <aashwin@arpmdesignandresearch.com>
2022-10-21 02:08:09 +0200

    SplitText also does not work

---
+++ TextLibrary/Operations/SplitText.cs
@@ -0,0 +1,54 @@
+﻿using Core;
+using System;
+using System.Windows;
+using Verse3;
+using Verse3.VanillaElements;
+
+namespace TextLibrary
+{
+    public class SplitText : BaseComp
+    {
+
+
+        #region Constructors
+
+        public SplitText() : base(0, 0)
+        {
+
+        }
+
+        public SplitText(int x, int y) : base(x, y)
+        {
+
+        }
+
+        #endregion
+
+        public override void Compute()
+        {
+            string a = this.ChildElementManager.GetData(nodeBlock, "Hello, You");
+            string splitter = this.ChildElementManager.GetData(nodeBlock2, ",");
+            
+            string[] splitArray = a.Split(splitter);
+            this.ChildElementManager.SetData(new DataStructure(splitArray).Data, nodeBlock2);    
+        }
+
+        public override CompInfo GetCompInfo() => new CompInfo(this, "Split Text", "Operations", "Text");
+
+        private TextDataNode nodeBlock;
+        private TextDataNode nodeBlock2;
+        private TextDataNode nodeBlock3;
+
+        public override void Initialize()
+        {
+            nodeBlock = new TextDataNode(this, NodeType.Input);      
+            this.ChildElementManager.AddDataInputNode(nodeBlock, "Text");
+
+            nodeBlock2 = new TextDataNode(this, NodeType.Input);
+            this.ChildElementManager.AddDataInputNode(nodeBlock2, "Split by");
+
+            nodeBlock3 = new TextDataNode(this, NodeType.Output);
+            this.ChildElementManager.AddDataOutputNode(nodeBlock3, "Split Text", true);
+        }
+    }
+}
